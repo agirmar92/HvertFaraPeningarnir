597a2ef2c9ba12c9e1acbe07e9bf93603f21568f
atlisg12 <atlisg12@ru.is>
2016-02-17 22:12:29 +0000

    bjútifæjað output úr ánni

--- database/river.py
+++ database/river.py
@@ -14,7 +14,6 @@ with open('results-3.csv', encoding='utf-16') as infile:
 	readr = csv.reader(infile, delimiter=';')
 	for x in readr:
 		rows.append([ y.strip() for y in x ])
-	print('hér!!!')
 
 docs = []
 doc = {}
@@ -23,11 +22,14 @@ for row in rows:
 	for i,column in enumerate(row):
 		doc[toprow[i]] = column
 	docs.append(doc)
-
+i = 0
 for doc in docs:
 	es.index(index="hvertfarapeningarnir", doc_type='tweet', body=doc)
+	if i % 10000 == 0:
+		print('Processing database.. ' + str(i) + ' documents created.')
+	i += 1
 
-print('Finished!')
+print('Finished! ' + str(i) + ' documents created.')
 
 res = es.search(index="hvertfarapeningarnir", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
