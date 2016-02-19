3ea0b80054559cfd72e04722dddd4555555a05d4
atlisg12 <atlisg12@ru.is>
2016-02-19 15:46:15 +0000

    áin gefur nú jafnmargar raðir og hún á að gera

--- database/river.py
+++ database/river.py
@@ -6,32 +6,35 @@ from getpass import getpass
 import csv
 from elasticsearch import Elasticsearch
 from pprint import pprint
+import time
 
 es = Elasticsearch()
-rows = []
 
+# Open the huge csv file and read from it
+rows = []
 with open('results-3.csv', encoding='utf-16') as infile:
 	readr = csv.reader(infile, delimiter=';')
 	for x in readr:
 		rows.append([ y.strip() for y in x ])
 
-
+# Convert to a list of dictionaries
 docs = []
 toprow = rows[0]
-for row in rows[2:]:
+for row in rows[2:-2]:
 	doc = {}
 	for i, column in enumerate(row):
 		doc[toprow[i]] = column
 	docs.append(doc)
 
-i = 0
-for doc in docs:
+# Process dictionaries into ElasticSearch
+for i, doc in enumerate(docs):
 	es.index(index="hvertfarapeningarnir", doc_type='db_entry', body=doc)
 	if i % 10000 == 0:
 		print('Processing database.. ' + str(i) + ' documents created.')
-	i += 1
+print('Finished! ' + str(len(docs)) + ' documents created.')
 
-print('Finished! ' + str(i) + ' documents created.')
-
+# Query the database, just for fun
+time.sleep(2)
 res = es.search(index="hvertfarapeningarnir", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
+
