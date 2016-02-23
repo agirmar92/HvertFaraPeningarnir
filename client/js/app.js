7477f5b2808746aacf5146d86711212f163f2908
agirmar92 <aegir13@ru.is>
2016-02-23 22:51:10 +0000

    progress bar, sidebar tabs and foundation for calendar

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,4 @@
-angular.module('hvertfarapeningarnir', ['ngRoute', 'angular-loading-bar']);
+angular.module('hvertfarapeningarnir', ['ngRoute', 'angular-loading-bar', 'ui.bootstrap']);
 
 angular.module('hvertfarapeningarnir')
 	.controller('testController', ['$scope', '$http', function ($scope, $http) {
@@ -6,6 +6,8 @@ angular.module('hvertfarapeningarnir')
 
 		$scope.testString = "Kópavogsbær";
 		$scope.testData = [];
+		$scope.dynamic = 80;
+		$scope.max = 100;
 
 		/*
 			asdasd
@@ -15,11 +17,28 @@ angular.module('hvertfarapeningarnir')
 				method: 'GET',
 				url: 'http://localhost:4000/'
 			}).success(function(response) {
-				$scope.testString = response.hits.hits[0]._source.Deild;
+				if ($scope.testString === "Kópavogsbær") {
+					$scope.testString = response.hits.hits[0]._source.Deild;
+					$scope.dynamic = 30;
+				} else {
+					$scope.testString = "Kópavogsbær";
+					$scope.dynamic = 80;
+				}
+
 				$scope.testData = response.hits.hits;
 				console.log(response);
 			}).error(function(err) {
 				console.log(err);
 			});
+		}
+
+		$scope.toggleDrawer = function() {
+			$("#wrapper").toggleClass("toggled");
+			$("#menu-toggle").toggleClass("glyphicon-remove").toggleClass("glyphicon-filter");
+		}
+
+		$scope.toggleCalendar = function() {
+			$("#hfp-calendar-dropdown").toggleClass("hfp-hidden");
+			$("#calendar-toggle").toggleClass("glyphicon-remove").toggleClass("glyphicon-calendar");
 		}
 	}]);
