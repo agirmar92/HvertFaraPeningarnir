a7f1a78bca5d96708f4d5695729d1d40b1dff123
atlisg12 <atlisg12@ru.is>
2016-03-19 00:23:51 +0000

    Default pie now has real data from server

--- server/api.js
+++ server/api.js
@@ -13,6 +13,11 @@ const elasticClient = new elasticsearch.Client({
 
 api.use(bodyParser.json());
 
+/* 
+*	This route will return two buckets from a aggregations in elasticSearch
+*	which are arrays of objects like so:
+*	[ {key:Affair, doc_count: number, sum_amount: {value}} ]
+*/
 api.get('/', (req, res) => {
 	/* Fetch all answers from database
 	models.Answer.find({}, (err, docs) => {
@@ -28,15 +33,70 @@ api.get('/', (req, res) => {
 	});*/
 
 	elasticClient.search({
-		index: 'hfp'
+		index: 'hfp',
+		body: {
+			"query": {
+		        "filtered": {
+		            "filter": {
+		                "range" : {
+		                    "Amount" : {
+		                        "gt" : 0
+		                    }
+		                }
+		            }
+		        }
+		    }, 
+		    "size": 0, 
+		    "aggs" : {
+		        "amounts" : {
+		            "terms": {
+		                "field": "Affair",
+		                "order": { "sum_amount": "desc" },
+		                "size": 0
+		            },
+		            "aggs" : {
+		                "sum_amount": { "sum": { "field": "Amount" } }
+		            }
+		        },
+		        "total_amount": { "sum": { "field": "Amount" }}
+		    }
+		}
 	}).then((doc) => {
 		//res.status(200).send(doc.hits.hits[0]._source);
-		res.status(200).send(doc);
+
+		const slices = doc.aggregations.amounts.buckets;
+		const totalCredit = doc.aggregations.total_amount.value;
+
+		elasticClient.search({
+			index: 'hfp',
+			body: {
+				"query": {
+			        "filtered": {
+			            "filter": {
+			                "range" : {
+			                    "Amount" : {
+			                        "lt" : 0
+			                    }
+			                }
+			            }
+			        }
+			    }, 
+			    "size": 0, 
+			    "aggs" : {
+			        "total_amount": { "sum": { "field": "Amount" }}
+			    }
+			}
+		}).then((docum) => {
+			const totalDebit = Math.abs(docum.aggregations.total_amount.value);
+			res.status(200).send({ slices, totalCredit, totalDebit });
+		}, (err) => {
+			console.log(err);
+			res.status(500).send('Server error\n');
+		});
 	}, (err) => {
 		console.log(err);
 		res.status(500).send('Server error\n');
 	});
-
 });
 
 module.exports = api;
