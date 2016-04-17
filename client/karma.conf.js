ba84d4e65f42dc0411584b209602ccfb12820686
Ubuntu <esja@hfpserver.up5zru2d3ksunhsfc2kh1ibbwb.fx.internal.cloudapp.net>
2016-04-17 16:57:42 +0000

    tests now publishing .xml results

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     reporters: ['progress', 'coverage'],
 
     coverageReporter: {
-        type: 'html',
+        type: 'cobertura',
         dir:  'coverage/'
     },
 
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'/*, 'Firefox', 'Safari'*/],
+    browsers: ['Chrome', /*'Firefox', 'Safari'*/],
 
 
     // Continuous Integration mode
