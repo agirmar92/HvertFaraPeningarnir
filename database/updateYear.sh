5e2441bc2d25e4f77a6f2ca5cd207a63aaebfa61
Darri <darrir13@ru.is>
2016-04-20 21:40:03 +0000

    River now creates index and processes data for each year that is specified by the user

---
+++ database/updateYear.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+
+curl -XDELETE "http://hfp.northeurope.cloudapp.azure.com:9200/hfp-$1"
+
+curl -XPUT    "http://hfp.northeurope.cloudapp.azure.com:9200/hfp-$1" -d'
+{
+    "settings": {
+        "number_of_replicas": 0,
+        "number_of_shards": 5,
+        "analysis": {
+            "analyzer": {
+                "uc_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "whitespace",
+                    "filter": [
+                        "lowercase",
+                        "pattern_a_filter",
+                        "pattern_d_filter",
+                        "pattern_e_filter",
+                        "pattern_i_filter",
+                        "pattern_o_filter",
+                        "pattern_u_filter",
+                        "pattern_y_filter",
+                        "pattern_th_filter",
+                        "pattern_ae_filter",
+                        "pattern_oo_filter",
+                        "pattern_uu_filter",
+                        "pattern_aa_filter",
+                        "pattern_ao_filter",
+                        "pattern_ss_filter",
+                        "pattern_tildan_filter",
+						"pattern_norskaoo_filter"
+                    ],
+                    "char_filter": [
+                        "my_pattern"
+                    ]
+                },
+				"card_analyzer": {
+					"type": "custom",
+					"tokenizer": "whitespace",
+					"filter": [
+						"pattern_star_filter"
+					]
+				}
+            },
+            "char_filter": {
+                "my_pattern": {
+                    "type": "pattern_replace",
+                    "pattern": " ",
+                    "replacement": ""
+                }
+            },
+            "filter": {
+                "pattern_a_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "á",
+                    "replacement": "az"
+                },
+                "pattern_d_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ð",
+                    "replacement": "dz"
+                },
+                "pattern_e_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "é",
+                    "replacement": "ez"
+                },
+                "pattern_i_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "í",
+                    "replacement": "iz"
+                },
+                "pattern_o_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ó",
+                    "replacement": "oz"
+                },
+                "pattern_u_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ú",
+                    "replacement": "uz"
+                },
+                "pattern_y_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ý",
+                    "replacement": "yz"
+                },
+                "pattern_th_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "þ",
+                    "replacement": "zz1"
+                },
+                "pattern_ae_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "æ",
+                    "replacement": "zz2"
+                },
+                "pattern_oo_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ö",
+                    "replacement": "zz3"
+                },
+                "pattern_uu_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ü",
+                    "replacement": "uz1"
+                },
+                "pattern_aa_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ä",
+                    "replacement": "az1"
+                },
+                "pattern_ao_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "å",
+                    "replacement": "az2"
+                },
+                "pattern_ss_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ß",
+                    "replacement": "sz"
+                },
+                "pattern_tildan_filter": {
+                    "type": "pattern_replace",
+                    "pattern": "ñ",
+                    "replacement": "nz"
+                },
+				"pattern_norskaoo_filter" : {
+					"type" : "pattern_replace",
+					"pattern" : "ø",
+					"replacement" : "zz3"
+				},
+				"pattern_star_filter" : {
+				  "type" : "pattern_replace",
+				  "pattern" : "\\*",
+				  "replacement" : "0"
+				}
+            }
+        }
+    },
+    "mappings": {
+        "doc": {
+            "properties": {
+            	"Date": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "AffairID": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "DepartmentID": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "FinanceKeyID": {
+                    "type": "integer"
+                },
+                "Amount": {
+                    "type": "long"
+                },
+                "FinanceKey": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "PrimaryFinanceKey": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "SecondaryFinanceKey": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "Department": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "Affair": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "Creditor": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "DepartmentGroup": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                },
+                "AffairGroup": {
+                    "type": "string",
+                    "index": "not_analyzed"
+                }
+            }
+        }
+    }
+}'
---
+++ database/yearRiver.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import json
+from datetime import datetime
+import pypyodbc
+from getpass import getpass
+import csv
+from elasticsearch import Elasticsearch
+from pprint import pprint
+import time
+# import certifi
+import argparse
+import subprocess
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("year", help="The year you are going to update.")
+args = parser.parse_args()
+
+user = 'admin'
+year = args.year
+
+createIndex = "/bin/bash updateYear.sh " + year
+os.system(createIndex)
+
+# p = subprocess.Popen(["./updateYear.sh", year])
+# print(p.pid)
+
+es = Elasticsearch(
+	['http://hfp.northeurope.cloudapp.azure.com:9200'],
+	#http_auth=(user, pswd),
+	port=9200,
+	verify_certs=True,
+	ca_certs=certifi.where(),
+)
+
+# Open the huge csv file and read from it
+rows = []
+with open('../results.csv', encoding='utf-16') as infile:
+	readr = csv.reader(infile, delimiter=';')
+	for x in readr:
+		rows.append([ y.strip() for y in x ])
+
+# Convert to a list of dictionaries
+docs = []
+cols = [ 'Date', 'AffairID', 'DepartmentID', 'FinanceKeyID', 'Amount', 'FinanceKey', 'PrimaryFinanceKey', 'SecondaryFinanceKey', 'Department', 'Affair', 'Creditor', 'DepartmentGroup', 'AffairGroup' ]
+#cols = rows[0]
+
+for row in rows[2:-2]:
+	doc = {}
+	for i, column in enumerate(row):
+		doc[cols[i]] = column
+	docs.append(doc)
+
+
+# Process dictionaries into ElasticSearch
+for i, doc in enumerate(docs):
+	#print(str(i) + ': ' + str(doc['Amount']))
+	es.index(index=year, doc_type='doc', body=doc)
+	if i % 10000 == 0:
+		print('Processing database.. ' + str(i) + ' documents created.')
+print('Finished! ' + str(len(docs)) + ' documents created.')
+
+# Query the database, just for fun
+time.sleep(2)
+res = es.search(index="hfp", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+
