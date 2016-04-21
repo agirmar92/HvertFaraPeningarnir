eced2c2deca06f579641315cf0e99ec734f8b51b
agirmar92 <aegir13@ru.is>
2016-04-21 11:23:14 +0000

    adding loading spinner to login

--- client/js/controllers/loginController.js
+++ client/js/controllers/loginController.js
@@ -1,10 +1,13 @@
 /**
  * Created by agirmar on 7.4.2016.
  */
-hfpApp.controller('loginController', function($scope, authenticationResource) {
+hfpApp.controller('loginController', function($scope, $rootScope, authenticationResource) {
+
+    $rootScope.loggingIn = false;
 
     $scope.login = function(form) {
         if (form.$valid) {
+            $rootScope.loggingIn = true;
             authenticationResource.login($scope.user);
         }
     };
