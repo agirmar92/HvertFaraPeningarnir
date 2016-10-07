86409609336801a90c557cbaa8c4a28c9a130486
darrir13 <darrir13@ru.is>
2016-10-07 21:59:45 +0000

    Update startClient.sh

--- startClient.sh
+++ startClient.sh
@@ -1,5 +1,6 @@
 #!/bin/bash          
 cd client
-python -m SimpleHTTPServer 8000
+live-server
+# python -m SimpleHTTPServer 8000
 # If you get an error message, use this instead:
 # python -m http.server 3000
